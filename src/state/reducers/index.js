2ec4779d52dc1749a2678b004a38520f3c11db7b
samroodAli <devsamrood@gmail.com>
2021-10-26 15:02:06 +0530

    Reducer:Create applicaton state with videos field

---
+++ src/state/reducers/index.js
@@ -0,0 +1,8 @@
+import { combineReducers } from "redux";
+import { videosReducer } from "./videosReducer";
+
+const reducers = combineReducers({
+  videos: videosReducer,
+});
+
+export default reducers;
