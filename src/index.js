f69f0e8a698f3abed255a1fb50232a9899e358c2
samroodAli <devsamrood@gmail.com>
2021-10-26 16:09:39 +0530

    Refactor: Refactor state to redux in application structure

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
 import { Provider } from 'react-redux';
-import redux from './state';
+import redux from './redux';
 import App from './components/App';
 
 ReactDOM.render(
