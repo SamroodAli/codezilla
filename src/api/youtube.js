6b3e9eb3ffa9ac0bf97b3275fbf60aaea652e905
samroodAli <devsamrood@gmail.com>
2021-10-26 16:44:29 +0530

    Render:VideoList and increase items count to 10

--- src/api/youtube.js
+++ src/api/youtube.js
@@ -6,7 +6,7 @@ export default Axios.create({
   baseURL: 'https://www.googleapis.com/youtube/v3',
   params: {
     part: 'snippet',
-    maxResults: 5,
+    maxResults: 10,
     type: 'video',
     key: KEY,
   },
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import SearchBar from './SearchBar';
+import VideoList from './VideosList';
 
 const App = () => (
   <div>
     <h1>Hello world</h1>
     <SearchBar />
+    <VideoList />
   </div>
 );
 
