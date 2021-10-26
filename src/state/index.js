9f57e5832603692b6fb56a3bfcd999bc4c1e9a73
samroodAli <devsamrood@gmail.com>
2021-10-26 15:10:55 +0530

    State:Export actionCreators and store from state folder

--- src/state/index.js
+++ src/state/index.js
@@ -1,2 +1,6 @@
-export * from "./store"
-export * as actionCreators from './action-creators'
+import { createStore } from "redux";
+import rootReducer from "./reducers";
+
+export * as actionCreators from "./action-creators";
+
+export default createStore(rootReducer);
