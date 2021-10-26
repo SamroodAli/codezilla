0277f49d9e7e829845a207b5fa1fc7134a295e6f
samroodAli <devsamrood@gmail.com>
2021-10-26 23:28:58 +0530

    Theme:Add theme state with reducer in app

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import videos from './videos';
 import currentVideo from './currentVideo';
+import theme from './theme';
 
 const reducers = combineReducers({
   videos,
   currentVideo,
+  theme,
 });
 
 export default reducers;
