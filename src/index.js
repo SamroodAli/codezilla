acf7ac2d4c45e09b8e4427822e84ef3116056f76
samroodAli <devsamrood@gmail.com>
2021-10-26 22:51:09 +0530

    Theme:Setup theme provider from theme-ui

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
 import { Provider } from 'react-redux';
+import { ThemeProvider } from 'theme-ui';
 import redux from './redux';
+import theme from './theme';
 import App from './components/App';
 
 ReactDOM.render(
   <Provider store={redux.store}>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </Provider>,
   document.getElementById('root'),
 );
