bb1ee27a2b08cce00a9736784b12f4ad81c1d3d8
samroodAli <devsamrood@gmail.com>
2021-10-27 09:56:57 +0530

    FilterOptions:Programmatically render filters using labels and checkboxes

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -1,12 +1,27 @@
+import { useSelector } from 'react-redux';
 import useActions from '../hooks/useActions';
+
+const COURSES = [
+  'React',
+  'Vue',
+  'Angular',
+  'Functional Programming',
+  'Microservices',
+  'JAM Stack',
+];
 
 const Filter = () => {
   const { setFilter } = useActions();
+  const filterState = useSelector(({ filters }) => filters);
+
   return (
     <form>
-      <input type="radio" id="css" onClick={() => setFilter('React')} />
-      <input type="radio" id="css" onClick={() => setFilter('Angular')} />
-      <input type="radio" id="css" onClick={() => setFilter('Vue')} />
+      {COURSES.map((course) => (
+        <label htmlFor="React" key={course}>
+          {course}
+          <input type="checkbox" id="css" onClick={() => setFilter(course)} checked={filterState[course]} />
+        </label>
+      ))}
     </form>
   );
 };
